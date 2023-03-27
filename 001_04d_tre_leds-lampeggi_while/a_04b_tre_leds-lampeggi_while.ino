4653a71e1037c2211df6fbd47ff6fa1f5259f749
aldopi <aldopi62@gmail.com>
2023-03-27 15:31:47 +0200

    Create a_04b_tre_leds-lampeggi_while.ino

---
+++ 001_04d_tre_leds-lampeggi_while/a_04b_tre_leds-lampeggi_while.ino
@@ -0,0 +1,49 @@
+/* *************************
+ *  by www.aldopi.it
+ *  corso BASE Arduino
+ *  v 1.0 - 31-10-2016
+ * *************************
+ */
+ 
+#define ledrosso  12
+#define ledgiallo 11
+#define ledverde  10
+#define tempo   3000
+
+int i = 0 ;  // dichiarazione variabile numero intero 2 byte 0..65535
+
+void setup() {
+  pinMode(ledrosso, OUTPUT);
+  pinMode(ledgiallo, OUTPUT);
+  pinMode(10, OUTPUT);
+}
+
+void loop() {
+  i = 1;
+  while (i < 10)  {  
+     // lampeggio led rosso 10 volte
+     digitalWrite(ledrosso, HIGH);
+     delay(tempo);
+     digitalWrite(ledrosso, LOW);
+     delay(tempo);
+     i++;
+  }
+  i = 1;
+  while (i < 5) {
+     // lampeggio led verde 5 volte
+     digitalWrite(ledverde, HIGH);
+     delay(tempo);
+     digitalWrite(ledverde, LOW);
+     delay(tempo);
+     i++
+  }
+  i = 1;
+  while (i < 20) {    
+     // lampeggio led giallo 20 volte
+     digitalWrite(ledgiallo, HIGH);
+     delay(tempo);
+     digitalWrite(ledgiallo, LOW);
+     delay(tempo);
+     i++;
+  }
+}
